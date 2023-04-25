2bd7a9797aee9f83282cf5d6d7d89a0a5db6ffd2
zxxng <103922121+zxxng@users.noreply.github.com>
2023-04-25 14:14:53 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.33 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    let arr = ''
+    for (let i of my_string) {
+        if (i === i.toUpperCase()) {
+            arr += i.toLowerCase();
+        } else {
+            arr += i.toUpperCase();
+        }
+    }
+    return arr;
+}
